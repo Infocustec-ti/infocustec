dd4ea76ee3e846fb8ea8a38a60a75ecbe3cd6abf
Infocustec-ti <atendimento@infocustec.com.br>
2024-10-05 17:58:59 -0300

    Update database.py

--- database.py
+++ database.py
@@ -93,11 +93,12 @@ class Usuario(Base):
 # Função para criar as tabelas no banco de dados
 def create_tables():
     try:
+        Base.metadata.drop_all(bind=engine)
         Base.metadata.create_all(bind=engine)
         logging.info("Tabelas criadas ou já existentes verificadas com sucesso.")
     except Exception as e:
         logging.error(f"Erro ao criar as tabelas: {e}")
-
+        print(f"Erro ao criar as tabelas: {e}")  # Log visível
 # Função para adicionar uma UBS ao banco de dados
 def add_ubs(nome_ubs):
     session = SessionLocal()
