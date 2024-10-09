01503517de3677abe0f32d286c55cabb4e7b857b
Infocustec-ti <atendimento@infocustec.com.br>
2024-10-08 22:50:01 -0300

    Update OS700.py

--- OS700.py
+++ OS700.py
@@ -540,10 +540,10 @@ def painel_chamados_tecnicos():
 
                 # Verificar se selected_rows retorna uma lista válida e se contém elementos
                 selected = grid_response['selected_rows']
-                
-                # Corrigir a captura da linha selecionada
-                if len(selected) > 0:
-                    chamado_selecionado = selected[0]
+
+                # Corrigir a captura da linha selecionada e garantir que seja tratada corretamente
+                if selected:
+                    chamado_selecionado = selected[0] if len(selected) > 0 else None
                 else:
                     chamado_selecionado = None
 
@@ -582,7 +582,6 @@ def painel_chamados_tecnicos():
             else:
                 st.info("Não há chamados em aberto no momento.")
                 logging.info("Nenhum chamado em aberto para exibir.")
-
         # Aba de Painel de Chamados
         with tab2:
             st.subheader('Painel de Chamados')
